4921c224c92b5a311d3772a1845dcb84c156a5cb
alexPietro <alexpietro.public@gmail.com>
2025-09-29 11:27:23 -0300

    a a a

--- server/controllers/controllerPaginaInicial.js
+++
@@ -1,3 +0,0 @@
-import {promises as fs} from 'fs';
-import {v4 as uuidv4} from 'uuid';
-
--- server/routers/router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import {
-    
-} from '../controllers/controllerPaginaInicial.js';
-
-const router = Router();
-
-
-export default router;
